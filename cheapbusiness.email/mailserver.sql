c4acf8acbe82d6111fe9c12364a5d39bf93543f6
root <root@ip-172-26-8-75.ec2.internal>
2023-06-04 17:07:04 -0400

    sql

---
+++ cheapbusiness.email/mailserver.sql
@@ -0,0 +1,144 @@
+-- MySQL dump 10.13  Distrib 8.0.33, for Linux (x86_64)
+--
+-- Host: localhost    Database: mailserver
+-- ------------------------------------------------------
+-- Server version	8.0.33-0ubuntu0.22.04.2
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `uid` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) DEFAULT NULL,
+  `password` text NOT NULL,
+  `domain_ids` text NOT NULL,
+  `auth_key` text,
+  `auth_exp` datetime DEFAULT NULL,
+  `plan` int DEFAULT '0',
+  `beta_user` int DEFAULT '1',
+  PRIMARY KEY (`uid`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'test@gmail.com','test','','869983bd6d3ea85cc35fc6a0c5701e43ebb5f4356942a4c6b8d371bf18731975dbce51b4f87e6481affa6dfdc052e9b46a5ac6414ac073ac27f847798d307aa2','2024-05-29 13:50:15',0,1),(5,'test2@gmail.com','test','','2b90f42b77017ef3a96a28bbe9e88236f2dc4fa68ac0601111ee90fbeb40644f34f96bb7db17d9f5475823b9fda0b98719b285ada611126045ecdcd8f69266f9','2024-05-21 10:15:30',0,1),(6,'nicholasconrad@icloud.com','test','','39aee49d5ae4a7565dc524e7820f136f2cb6ce0116add54ba92501898e79d3a1f7fef22c2cfd4b529dc6f42a7ae66af37ae646a2aa83ae4c9d36535f167f4cc4','2024-05-27 18:54:41',0,1),(7,'','','','0a693e5fa6b3047f14bd7724dda4da629284d67fc285f800c1b3a8a48d6e5e21b17325faefad38ce29f3b678834fc25daa4eed7cffa50b236735d375af642aff','2024-05-27 03:43:26',0,1),(11,'test3@gmail.com','$2b$11$mRfM9t5bMR425qTaNjELSegAs6MtGh7ERDgmvN45GaVU7tIeR7nXe','','16a9a6754e58992e75952b0eabe37d737fb02f306d7b054e7a12434b6127bac4bb8a284ced7e8d878a9c2f0bdcd9731014ec9a78865718cb5b5b1a546f5a00e0','2024-06-03 15:37:04',0,1);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `virtual_aliases`
+--
+
+DROP TABLE IF EXISTS `virtual_aliases`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `virtual_aliases` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `domain_id` int NOT NULL,
+  `source` varchar(100) NOT NULL,
+  `destination` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `domain_id` (`domain_id`),
+  CONSTRAINT `virtual_aliases_ibfk_1` FOREIGN KEY (`domain_id`) REFERENCES `virtual_domains` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `virtual_aliases`
+--
+
+LOCK TABLES `virtual_aliases` WRITE;
+/*!40000 ALTER TABLE `virtual_aliases` DISABLE KEYS */;
+/*!40000 ALTER TABLE `virtual_aliases` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `virtual_domains`
+--
+
+DROP TABLE IF EXISTS `virtual_domains`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `virtual_domains` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `owner_uid` int DEFAULT NULL,
+  `dns_verified` int DEFAULT '0',
+  `dns_txt_code` text,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `virtual_domains`
+--
+
+LOCK TABLES `virtual_domains` WRITE;
+/*!40000 ALTER TABLE `virtual_domains` DISABLE KEYS */;
+INSERT INTO `virtual_domains` VALUES (8,'rise-game.com',1,1,'97b66f0466a772d939025a0557f75eb42634b8eada7e5d31b3ac900e7198d3163aea167492cd708113262ebf42bad8f951901e07bceacab843ae9faab976dfa7'),(19,'cheapbusiness.email',6,0,'21bc363de0f51221a3479ab77a621b0a9bf439fa4c52f7672d7c5039f453ae54a594d0264df7aa0ce6ed2b1bda21764bc80df928f82597c23e6236a3fce89a0f'),(22,'v.com',11,1,'3c78209d0330601c3f5b70e67c442ddb78406f8128f2456ecbd46b43d8c5b210b931845668217d3d380070f9c754c41440bf32f5d721760152a0fa53a13b7f30');
+/*!40000 ALTER TABLE `virtual_domains` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `virtual_users`
+--
+
+DROP TABLE IF EXISTS `virtual_users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `virtual_users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `domain_id` int NOT NULL,
+  `password` varchar(106) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `email_2` (`email`),
+  KEY `domain_id` (`domain_id`),
+  CONSTRAINT `virtual_users_ibfk_1` FOREIGN KEY (`domain_id`) REFERENCES `virtual_domains` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `virtual_users`
+--
+
+LOCK TABLES `virtual_users` WRITE;
+/*!40000 ALTER TABLE `virtual_users` DISABLE KEYS */;
+INSERT INTO `virtual_users` VALUES (6,22,'$6$Dd4RqJyEmBvR3vXb$5dr2cTwqSzW8oUOHTPoVRuyIs1WD.k4idgcuRmbYKxGE1NOddP1B0YtmhKc4jkgK4LM2O27f/VAecteDukacw/','2@v.com','2023-05-30 20:05:16'),(11,22,'$6$OjdzwJoyMho1W1j8$/g5EaLqaxGDfQJs/ou//Ftn/Fms88uG4yY7NALhYTx4a0wRlx1GSkKCZu6xC2z.stJI5/iVBfJQKKlaZ0P6pG/','1@v.com','2023-06-04 19:33:19');
+/*!40000 ALTER TABLE `virtual_users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-06-04 21:05:49
