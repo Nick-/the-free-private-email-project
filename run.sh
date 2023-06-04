a8b520f62486da0629e659ce98ba2d59883b85b0
Nick- <nicholasconrad96@gmail.com>
2023-06-04 18:29:54 -0400

    r

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 sudo bash kac.sh
 sudo docker run -v /etc/letsencrypt/:/etc/letsencrypt/ \
- -v /home/ubuntu/email/cheapbusiness.email/:/cheapbusiness.email/ \
+ -v ~/email/cheapbusiness.email/:/cheapbusiness.email/ \
  -v mailsql:/var/lib/mysql \
- -v /home/ubuntu/email/mailbox/:/var/mail/ \
+ -v ~/email/mailbox/:/var/mail/ \
  -it --name email-server \
  -p 3306:3306 -p 8080:8080 -p 25:25 -p 587:587 \
  -p 143:143 -p 465:465 -p 993:993 -p 995:995 \
