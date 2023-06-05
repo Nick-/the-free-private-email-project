6b35712dc5cbc50b113b5df171ee6bac528d3c1f
Nicholas <nicholas@radd.tech>
2023-06-05 15:57:08 -0400

    dovecot permissions

--- s.sh
+++ s.sh
@@ -1,6 +1,8 @@
 groupadd -g 5000 vmail
 useradd -g vmail -u 5000 vmail -d /var/mail
 chown -R vmail:vmail /var/mail
+chown -R vmail:dovecot /etc/dovecot
+chmod -R o-rwx /etc/dovecot
 
 usermod -d /var/lib/mysql/ mysql
 /etc/init.d/mysql start
