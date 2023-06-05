bf246abc3660716a2a1d78aa85954449396abbe5
Nicholas <nicholas@radd.tech>
2023-06-05 16:51:01 -0400

    dkim rollback

--- s.sh
+++ s.sh
@@ -1,15 +1,15 @@
 groupadd -g 5000 vmail
 useradd -g vmail -u 5000 vmail -d /var/mail
 chown -R vmail:vmail /var/mail
-chown -R vmail:dovecot /etc/dovecot
-chmod -R o-rwx /etc/dovecot
+#chown -R vmail:dovecot /etc/dovecot
+#chmod -R o-rwx /etc/dovecot
 
 usermod -d /var/lib/mysql/ mysql
 /etc/init.d/mysql start
 #mysql < mailDB.sql
 postfix start
 dovecot
-opendkim
+#opendkim
 cd cheapbusiness.email
 nohup node WebServer.js 2> /dev/null &
 #tail -f /var/log/mail.log
