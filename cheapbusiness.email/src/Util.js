8f30b0a134315f944b8be7550b1e4d44341af5b6
Nicholas <nicholas@radd.tech>
2023-06-12 10:32:34 -0400

    debug

--- cheapbusiness.email/src/Util.js
+++ cheapbusiness.email/src/Util.js
@@ -386,7 +386,7 @@ async function addEmailUser(user_data, full_email, mailbox_size_gb, c) {
         }
 
         var mailbox_gb_remaining = mailbox_gb_allowed - user_data.mailbox_gb_allocated;
-
+        console.log("Subtracting " + mailbox_size_gb + " from remaining storage: " + mailbox_gb_remaining)
         if(mailbox_size_gb - mailbox_gb_remaining < 0) {
             resolve({ status: "failed", error: "Not enough storage, please upgrade!" })
             return 
