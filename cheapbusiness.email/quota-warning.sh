67f9452e43e7edda34ea67a0bde7c6020a9d9177
Nicholas <nicholas@radd.tech>
2023-06-12 15:20:24 -0400

    q

--- cheapbusiness.email/quota-warning.sh
+++ cheapbusiness.email/quota-warning.sh
@@ -2,5 +2,4 @@
 PERCENT=$1
 USER=$2
 MESSAGE="$(<quota-warning-template.html)"
-echo "{$MESSAGE//[[pct]]/$PERCENT}" | msmtp -a default $USER
-
+echo "${MESSAGE//pct/$PERCENT}" | msmtp -a default $USER
