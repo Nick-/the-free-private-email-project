fbc50670072478f4f7e49e8bf984d71ef8dde0e0
Nicholas <nicholas@radd.tech>
2023-06-12 14:28:23 -0400

    q

--- cheapbusiness.email/quota-warning.sh
+++ cheapbusiness.email/quota-warning.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 PERCENT=$1
 USER=$2
-SUBJECT="Test Subject"
-MESSAGE="
-From: Cheap Business Email <noreply@cheapbusiness.email>
-Subject: Your mailbox is almost full!
-
-Your Mailbox is currently $PERCENT % Full!
-"
-
+MESSAGE="From: Cheap Business Email <noreply@cheapbusiness.email>\nSubject: Your mailbox is $PERCENT% Full!\n\nYour Mailbox is currently $PERCENT% Full!"
 echo $MESSAGE | msmtp -d $USER
