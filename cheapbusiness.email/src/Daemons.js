1adac10c9ece40383bbdff5cbdc2171ec707fb2a
Nick- <nicholasconrad96@gmail.com>
2023-06-18 09:17:21 -0400

    Keep Alive

--- cheapbusiness.email/src/Daemons.js
+++ cheapbusiness.email/src/Daemons.js
@@ -35,7 +35,12 @@ DB.con.query(cpeq, (error, results) => {
 }
 
 function hourDaemon() {
-
+    var ka = "SELECT uid FROM users LIMIT 1"; //keepAlive
+    DB.con.query(ka, (error, results) => {
+        if (error) {
+            reportError(error.stack)
+        } 
+    });
 }
 
 function minuteDaemon() {
