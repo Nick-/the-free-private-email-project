5eda4d0d54229fd505c21b99cb220d503323aff9
Nicholas Conrad <nicholasconrad96@gmail.com>
2024-02-08 14:09:56 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 read -p "Enter Email Domain: " domain
 echo "Setting Up Dockerfile for nicholas@$domain"
 
-find . -name '*' -exec sed -i -e "s/cheapbusiness.email/$domain/g" {} \;
+find . -name '*' -type f -exec sed -i -e "s/cheapbusiness.email/$domain/g" {} \;
 
 
