c6c936d84840e73fb4ec53e865c3e0f1ab7ba514
Nicholas Conrad <nicholasconrad96@gmail.com>
2024-02-09 13:10:43 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,12 @@
-read -p "Enter Email Domain: " domain
-echo "Setting Up Dockerfile for nicholas@$domain"
+read -p "Enter Email Domain (cheapbusiness.email): " domain
 
+if [ -z "$domain" ]
+then
+      echo "Using default domain (cheapbusiness.email)"
+      domain='cheapbusiness.email'
+fi
+
+echo "Setting Up Dockerfile for $domain"
 find . -name '*' -type f -exec sed -i -e "s/cheapbusiness.email/$domain/g" {} \;
-
 cd cheapbusiness.email
 npm install --save
