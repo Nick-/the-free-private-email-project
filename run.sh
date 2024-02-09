0d78bb32b6935499e97372d4cad494f3eee3adab
Nicholas Conrad <nicholasconrad96@gmail.com>
2024-02-09 20:06:01 +0000

    d

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@ sudo bash kac.sh
 sudo docker run -v /etc/letsencrypt/:/etc/letsencrypt/ \
  -v ~/email/cheapbusiness.email/:/cheapbusiness.email/ \
  -v ~/email/mailbox/:/var/mail/ \
- -it --name email-server \
+ -d --name email-server \
  -p 8080:8080 -p 25:25 -p 587:587 \
  -p 143:143 -p 465:465 -p 993:993 -p 995:995 \
  ncc15c/email 
--- s.sh
+++ s.sh
@@ -7,4 +7,4 @@ cd cheapbusiness.email
 nohup node WebServer.js 2> /dev/null &
 
 #This keeps the container running in -d mode.
-#tail -f /var/log/mail.log
+tail -f /var/log/mail.log
